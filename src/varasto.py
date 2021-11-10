5fd2a9d8bc1191f322ba596013a46e4bc85bc810
Katri <katri.rytkonen@outlook.com>
2021-11-10 12:43:55 +0200

    Viikko 2: Tehtävä 4: GitHub Actions, koodi on rikottu

--- src/varasto.py
+++ src/varasto.py
@@ -38,4 +38,4 @@ class Varasto:
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}" #breaking the rules breaking the rules breaking the rules breaking the rulesbreaking the rulesbreaking the rules 
